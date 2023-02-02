90adcb4ec27801ea0930ebd524281c4824130e48
2jigoo <2jigoo@naver.com>
2023-02-02 15:07:02 +0900

    feat: init

---
+++ src/test/java/com/jigoo/practice/PracticeSpringGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jigoo.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
